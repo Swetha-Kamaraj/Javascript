aa6f679c87f006954c498c07c86ce6268cf5587d
Swetha Kamaraj <86401490+Swetha-Kamaraj@users.noreply.github.com>
2024-11-11 19:35:44 +0530

    Add files via upload

---
+++ Javascript/Sample/sample.js
@@ -0,0 +1,12 @@
+// function area(l,b){
+//     console.log("The area is:"+l*b)
+// }
+// area(2,2)
+// for(i=1;i<=5;i++){
+//     console.log("Swetha")
+// }
+// for(i=1;i<=5;i++){
+//     console.log(i+"*"+"5 = "+i*5)
+// }
+var a=Math.random()
+console.log(a*10)
